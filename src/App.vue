03aa6dc84a65578c0ac7b7b815fa2b18e1b991a9
Bowske <adabor6@st.amu.edu.pl>
2020-10-13 21:08:01 +0200

    app bar done - prolly should use grid more instead of flexbox but idk

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-navigation-drawer v-model="isDrawerOpen" app>
+      <!-- -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon
+        @click="isDrawerOpen = !isDrawerOpen"
+      ></v-app-bar-nav-icon>
+      <div class="d-flex">
+        <v-icon>mdi-calendar</v-icon>
+        <span class="pl-2">Kalendarz</span>
+      </div>
+      <v-row class="d-flex justify-space-between">
+        <div class="d-flex align-center pl-12">
+          <v-btn class="mr-2" outlined small>Dzisiaj</v-btn>
+          <div class="pr-2">
+            <v-btn icon small><v-icon small>mdi-chevron-left</v-icon></v-btn>
+            <v-btn icon small><v-icon small>mdi-chevron-right</v-icon></v-btn>
+          </div>
+          <span>Październik 2020</span>
+        </div>
+
+        <div>
+          <v-btn icon small
+            ><v-icon small>mdi-help-circle-outline</v-icon></v-btn
+          >
+          <v-menu bottom right>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                class="ml-3"
+                small
+                outlined
+                color="grey darken-2"
+                v-bind="attrs"
+                v-on="on"
+              >
+                <span>Month</span>
+                <v-icon right> mdi-menu-down </v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item @click="type = 'day'">
+                <v-list-item-title>Day</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = 'week'">
+                <v-list-item-title>Week</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = 'month'">
+                <v-list-item-title>Month</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = '4day'">
+                <v-list-item-title>4 days</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+          <v-avatar class="ml-4" size="32" color="purple"></v-avatar>
+        </div>
+      </v-row>
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid> </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {},
+
+  data: () => ({
+    isDrawerOpen: false,
+    //
+  }),
+};
+</script>
